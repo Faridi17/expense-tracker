c7329894953e841ff84656763b1f2788708bab37
Muhammad Azka Faridi <122891024+Faridi17@users.noreply.github.com>
2025-03-17 00:21:48 +0700

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -10,13 +10,7 @@ import { Platform } from "react-native";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCD-O9Mb5yx0sciUodxMYUp9aY01axnPpY",
-  authDomain: "expense-tracker-89b4b.firebaseapp.com",
-  projectId: "expense-tracker-89b4b",
-  storageBucket: "expense-tracker-89b4b.firebasestorage.app",
-  messagingSenderId: "357120143142",
-  appId: "1:357120143142:web:e2b89b232b6bce73d25be7",
-  measurementId: "G-F3JSXHCB8E"
+  // config
 };
 
 // Inisialisasi Firebase
